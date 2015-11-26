0b64192bbf168b9f18254733ac5008def35272d5
Oleg <darelvc@gmail.com>
2015-11-27 00:22:16 +0200

    Add validates form and episode mp3

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -4,8 +4,11 @@ class Episode < ActiveRecord::Base
   has_attached_file :episode_thumbnail, styles: { large: "1000x1000#", medium: "550x550#" }
   validates_attachment_content_type :episode_thumbnail, content_type: /\Aimage\/.*\Z/
 
-  validates :title, :description, presence: true
-  validates :title, uniqueness: true
-  validates :title, length: {minimum: 5}
-  validates :description, length: {minimum: 30}
+  has_attached_file :mp3
+  validates_attachment :mp3, :content_type => { :content_type => ["audio/mpeg", "audio/mp3"] }, :file_name => { :matches => [/mp3\Z/] }
+
+  #validates :title, :description, presence: true
+  #validates :title, uniqueness: true
+  #validates :title, length: {minimum: 5}
+  #validates :description, length: {minimum: 30}
 end
