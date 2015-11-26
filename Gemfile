c52edc1c8b1f4b440029918618288699e9c197ee
Oleg <darelvc@gmail.com>
2015-11-26 09:56:24 +0200

    Add validates to podcast controller

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'devise', '~> 3.5', '>= 3.5.2'
 gem 'paperclip', '~> 4.3', '>= 4.3.2'
+gem 'aws-sdk', '~> 2.2', '>= 2.2.3'
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
 
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,4 +3,9 @@ class Episode < ActiveRecord::Base
 
   has_attached_file :episode_thumbnail, styles: { large: "1000x1000#", medium: "550x550#" }
   validates_attachment_content_type :episode_thumbnail, content_type: /\Aimage\/.*\Z/
+
+  validates :title, :description, presence: true
+  validates :title, uniqueness: true
+  validates :title, length: {minimum: 5}
+  validates :description, length: {minimum: 30}
 end
