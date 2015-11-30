5106c28c7d8b91c4f23103b6b43f86fc182f8901
Oleg <darelvc@gmail.com>
2015-11-30 15:38:16 +0200

    Add mp3 player and enable validations on edit page

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'devise', '~> 3.5', '>= 3.5.2'
 gem 'paperclip', '~> 4.3', '>= 4.3.2'
 gem 'aws-sdk', '~> 2.2', '>= 2.2.3'
+gem 'font-awesome-sass'
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
 
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -4,11 +4,13 @@ class Episode < ActiveRecord::Base
   has_attached_file :episode_thumbnail, styles: { large: "1000x1000#", medium: "550x550#" }
   validates_attachment_content_type :episode_thumbnail, content_type: /\Aimage\/.*\Z/
 
-  has_attached_file :mp3
-  validates_attachment :mp3, :content_type => { :content_type => ["audio/mpeg", "audio/mp3"] }, :file_name => { :matches => [/mp3\Z/] }
+  #has_attached_file :mp3
+  #validates_attachment :mp3, :content_type => { :content_type => ["audio/mpeg", "audio/mp3"] }, :file_name => { :matches => [/mp3\Z/] }
+  has_attached_file :mp3, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio', 'audio/wav' ]
+  validates_attachment :mp3, :content_type => { :content_type => ["audio/wav", "audio/mpeg", "audio/mp3"] }, :file_name => { :matches => [/mp3\Z/, /wav\Z/] }
 
-  #validates :title, :description, presence: true
-  #validates :title, uniqueness: true
-  #validates :title, length: {minimum: 5}
-  #validates :description, length: {minimum: 30}
+  validates :title, :description, presence: true
+  validates :title, uniqueness: true
+  validates :title, length: {minimum: 5}
+  validates :description, length: {minimum: 30}
 end
